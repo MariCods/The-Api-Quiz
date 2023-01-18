6caf91483ecf19c936f7a5101df93b20fb963cd5
Mary Cods <maryam.selassie@gmail.com>
2023-01-18 03:00:01 -0800

    HTML Update

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
-let question1 = {
-    quest1: 'What are the three main building blocks for coding a website?',
-    solutions:['html css avascript', 'css python javascript', 'web apis  html css'],
-    correctAnswer: 2
-};
 
-function showQuestion(q1){
-    let quest1Div = document.getElementById('quest1');
-    quest1Div.textcontent = q1.title
 
-    let sol = document.querySelectorAll('.solutions');
-    console.log(sol);
+
+
+
+
+// let question1 = {
+//     quest1: 'What are the three main building blocks for coding a website?',
+//     solutions:['html css avascript', 'css python javascript', 'web apis  html css'],
+//     correctAnswer: 2
+// };
+
+// function showQuestion(q1){
+//     let quest1Div = document.getElementById('quest1');
+//     quest1Div.textcontent = q1.title
+
+//     let sol = document.querySelectorAll('.solutions');
+//     console.log(sol);
     
-    sol.forEach(function(element, index){
-        element.textcontent = q1.solutions[index];
-    })
-};
+//     sol.forEach(function(element, index){
+//         element.textcontent = q1.solutions[index];
+//     })
+// };
 
-showQuestion(question1);
+// showQuestion(question1);
