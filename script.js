ff8ef81b4f04113d413489f6cec99945e5326c60
Mary Cods <maryam.selassie@gmail.com>
2023-01-18 00:00:45 -0800

    First Commit

---
+++ script.js
@@ -0,0 +1,19 @@
+let question1 = {
+    quest1: 'What are the three main building blocks for coding a website?',
+    solutions:['html css avascript', 'css python javascript', 'web apis  html css'],
+    correctAnswer: 2
+};
+
+function showQuestion(q1){
+    let quest1Div = document.getElementById('quest1');
+    quest1Div.textcontent = q1.title
+
+    let sol = document.querySelectorAll('.solutions');
+    console.log(sol);
+    
+    sol.forEach(function(element, index){
+        element.textcontent = q1.solutions[index];
+    })
+};
+
+showQuestion(question1);
