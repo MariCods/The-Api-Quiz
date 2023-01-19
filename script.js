1fcbdd1c8fb30346b3f768491a43eba3afd28f9d
Mary Cods <maryam.selassie@gmail.com>
2023-01-19 02:23:58 -0800

    JS Update

--- script.js
+++ script.js
@@ -1,4 +1,111 @@
+function move() {
+    const element = document.getElementById("myBar");
+    let width = 0;
+    const id = setInterval(frame, 1000);
+    function frame() {
+        if(width == 50) {
+            clearInterval(id);
+        } else {
+            width++;
+            element.style.width = width + '%';
+        }
+    }
+}
 
+const questionEL = document.getElementById("question");
+const choices = Array.from(document.getElementsByClassName("choice-text"));
+console.log(choices);
+
+
+
+//let currentQuestion = [];
+let acceptingSelections = true;
+let score = 0;
+let questionCounter = 0;
+let availableSelections = [];
+var currentQuestionindex = 0;
+
+let questions = [
+    {
+        question: "What is a Boolean expression?",
+
+        choice1:"It has 2 possible paths, one is taken if the condition is true and the other is if its false",
+        choice2:"When the condition is false",
+        choice3:"An expression that can be evaluated as either true or false",
+        answer:3
+    },
+    {
+        question: "Inside which HTML element do we put the JavaScript?",
+
+        choice1:" <js>",
+        choice2:"<script>",
+        choice3:"<scripting>",
+        answer:2
+    },
+    {
+        question: "How do you create a function?",
+
+        choice1:"function myFunction()",
+        choice2:"function:myFunction()",
+        choice3:"myFunction():function",
+        answer:1
+    },
+    {
+        question: " A named element in a JavaScript program that is used to store and retrieve data is a _____.",
+
+        choice1:"Variable",
+        choice2:"Method",
+        choice3:"String",
+        answer:1
+    },
+    {
+        question: "The majority of a typical Web document will be found in:",
+
+        choice1:"The title tag",
+        choice2:"The head tag",
+        choice3:"The body tag",
+        answer:3
+    },
+    {
+        question: "When you want to use JavaScript to manipulate the browser window, the browser window's JavaScript object name is:",
+
+        choice1:"browser_window",
+        choice2:"document",
+        choice3:"Window",
+        answer:3
+    }
+];
+
+
+const correctTotal = 15;
+const MaxQuestions = 6;
+
+function getCurrentQuestion(index){
+    const currentQuestion = questions[index];
+    questionEL.textContent = currentQuestion.question;
+
+    choices.forEach(function(choice,index){
+        choice.textContent = currentQuestion["choice"+ (index + 1)]
+    })
+}
+
+for (let choice of choices) {
+    choice.addEventListener("click", function(event) {
+        const clickedQuestion = event.target;
+        if (clickedQuestion.dataset.number == questions[currentQuestionindex].answer) {
+            // correct
+        } else {
+            // wrong
+         
+            currentQuestionindex++;
+            return getCurrentQuestion(currentQuestionindex);
+        }  
+       
+    });
+}
+
+//console.log(currentQuestion);
+ getCurrentQuestion(currentQuestionindex);
 
 
 
