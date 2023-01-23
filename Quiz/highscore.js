e61f98f40295c8c05a0477b4017790154ed46b18
Mary Cods <maryam.selassie@gmail.com>
2023-01-22 17:51:45 -0800

    Fixed List

--- Quiz/highscore.js
+++ Quiz/highscore.js
@@ -1,35 +1,27 @@
 
 var gamePlayers = document.querySelector('#player-list');
 
-var scores = [];
+var scores = JSON.parse(localStorage.getItem('High-Scores'));
 
 function renderScores() {
-    // Clear todoList element and update todoCountSpan
-    gamePlayer = document.getElementById('player-list').innerText = localStorage.getItem('High-Scores');
+   
+    // gamePlayer = document.getElementById('player-list').innerText = localStorage.getItem('High-Scores');
     // scores.textContent = gamePlayers.length;
   
-    // Render a new li for each todo
-   for (var i = 0; i < gamePlayers.length; i++) {
+    
+   for (var i = 0; i < scores.length; i++) {
         // scores = gamePlayers[i];
   
-    //  var li = document.createElement("li");
-    //   li.textContent = scores;
+     var li = document.createElement("li");
+      li.textContent = scores[i].intials + ':' + scores[i].score;
     //  li.setAttribute(data-number, i);
   
-    //  scores.appendChild(li);
+      gamePlayers.appendChild(li);
    }
   }
 
-//    function inIt() {
 
-   
-//      if (gamePlayers !== null) {
-//         scores = '';
-//      }
       renderScores();
-//   }
-
-// inIt()
 
 
 
