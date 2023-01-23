a69ae634bff890b61ee5fb81e3b74d194ae23ab9
Mary Cods <maryam.selassie@gmail.com>
2023-01-22 23:20:37 -0800

    Final Edits

--- Quiz/highscore.js
+++ Quiz/highscore.js
@@ -4,24 +4,24 @@ var gamePlayers = document.querySelector('#player-list');
 var scores = JSON.parse(localStorage.getItem('High-Scores'));
 
 function renderScores() {
-   
-    // gamePlayer = document.getElementById('player-list').innerText = localStorage.getItem('High-Scores');
-    // scores.textContent = gamePlayers.length;
-  
-    
-   for (var i = 0; i < scores.length; i++) {
-        // scores = gamePlayers[i];
-  
-     var li = document.createElement("li");
-      li.textContent = scores[i].intials + ':' + scores[i].score;
+
+  // gamePlayer = document.getElementById('player-list').innerText = localStorage.getItem('High-Scores');
+  // scores.textContent = gamePlayers.length;
+
+
+  for (var i = 0; i < scores.length; i++) {
+    // scores = gamePlayers[i];
+
+    var li = document.createElement("li");
+    li.textContent = scores[i].intials + ':' + scores[i].score;
     //  li.setAttribute(data-number, i);
-  
-      gamePlayers.appendChild(li);
-   }
+
+    gamePlayers.appendChild(li);
   }
+}
 
 
-      renderScores();
+renderScores();
 
 
 
